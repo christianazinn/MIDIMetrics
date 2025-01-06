e2da6215227ed31c59bb452dff246c6fabaeb625
Davide <davide.rizzotti@mail.polimi.it>
2025-01-06 19:43:56 +0100

    fix output folder

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -9,12 +9,12 @@ from tests_utils import HERE
 MIDI_PATHS = list((HERE / "midis").glob("**/*.midi"))
 
 metric_config = MetricConfig(
-    #bar_absolute_pitches= True,
+    bar_absolute_pitches= True,
     bar_pitch_variety=True,
-    #bar_note_density=True,
-    #note_durations_set=True
+    bar_note_density=True,
+    note_durations_set=True,
     #note_durations_frequency=True #TODO! (maybe remove)
-    #ngrams_repetitions=True,
+    ngrams_repetitions=True,
     polyphony_min_max=True
 )
 
